6108fa1b2c4dda3f3ad8f1010293de51269a5dbf
Sameer Nawaz <sameer.nwaz@gmail.com>
2020-12-30 19:31:46 +0530

    Initial Commit

---
+++ SwiftUI-Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Animations
+//
+//  Created by Sameer Nawaz on 30/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Animations/SwiftUI_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationsApp.swift
+//  SwiftUI-Animations
+//
+//  Created by Sameer Nawaz on 30/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
